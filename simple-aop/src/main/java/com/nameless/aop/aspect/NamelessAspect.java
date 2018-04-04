cad37c3306553cd029403f93b003f3a922073c49
stanleysun <stanleysun@ehsure.com>
2018-04-04 08:02:23 +0800

    init aop

--- simple-aop/src/main/java/com/nameless/aop/aspect/NamelessAspect.java
+++ simple-aop/src/main/java/com/nameless/aop/aspect/NamelessAspect.java
@@ -1,10 +1,32 @@
 package com.nameless.aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Thinkpad on 2018/4/3.
  */
-@Aspect
 public class NamelessAspect {
+
+    private static Logger logger = LoggerFactory.getLogger(NamelessAspect.class);
+
+    /**
+     * method of aop around
+     * @param proceedingJoinPoint
+     * @return
+     */
+    public Object namelessAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+
+        Object[] params = proceedingJoinPoint.getArgs();
+        logger.info(String.format("method [%s] input parameters is [%s]",proceedingJoinPoint.toString(),params));
+        Object result = null ;
+        try {
+            result = proceedingJoinPoint.proceed(params);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return result ;
+    }
 }
