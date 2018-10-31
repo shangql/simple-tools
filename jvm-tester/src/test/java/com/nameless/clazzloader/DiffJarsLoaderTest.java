2855258619ebd37ee762adeee4dc7485a71fe6af
stanley <boyszh1982@163.com>
2018-10-31 17:06:17 +0800

    test

--- jvm-tester/src/test/java/com/nameless/clazzloader/DiffJarsLoaderTest.java
+++ jvm-tester/src/test/java/com/nameless/clazzloader/DiffJarsLoaderTest.java
@@ -13,7 +13,12 @@ public class DiffJarsLoaderTest {
 
     /**
      * 测试1.0版本和2.0版本共同加载时是否报错
-     * getListBySth只有jvm-tester-2.0-SNAPSHOT.jar中存在此方法
+     * getListBySth只有jvm-tester-2.0-SNAPSHOT.jar中存在此方法*
+     * 最终会拼成 file://${绝对路径}*
+     * //因为会放入Set<T>中，所以顺序是随机的，根据hashCode有关。*
+     * 解释了如果路径变化，则hashCode会变化。
+     * 如果低版本先加载了，那么新版本就会被排除，这样调用新版本的方法就会出现错误。
+     * BTW:顺序可以通过调整pom.xml中DIFF*.jar的顺序来测试。
      */
     @Test
     public void test() {
