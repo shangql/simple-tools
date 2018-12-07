70b5c00691c8d56b3ca6d01e26c89b0b992371e0
stanley <boyszh1982@163.com>
2018-12-07 12:26:04 +0800

    xx

--- apache-cxf-case/src/main/java/com/nameless/HttpPostClient.java
+++ apache-cxf-case/src/main/java/com/nameless/HttpPostClient.java
@@ -1,5 +1,6 @@
 package com.nameless;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -7,8 +8,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.Base64;
 
 /**
@@ -19,20 +23,33 @@ public class HttpPostClient {
 
     public static void main(String[] args) throws Exception {
 
+        HttpPostClient httpPostClient = new HttpPostClient();
+        ;
+
         String username = System.getProperty("username");
         String password = System.getProperty("password");
         System.out.println(String.format("username=%s,password=%s.",username,password));
 
-        String queryxml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:glob=\"http://sap.com/xi/SAPGlobal20/Global\" xmlns:y62=\"http://0001092235-one-off.sap.com/Y628OLNKY_\" xmlns:yni=\"http://0001092235-one-off.sap.com/YNIIVJHSY_\"> <soapenv:Header/> <soapenv:Body><n0:ServiceOrderByElementsQuery_SYNC xmlns:n0=\"http://sap.com/xi/SAPGlobal20/Global\"> <ServiceOrderSelectionByElements> <SelectionByID> <InclusionExclusionCode>I</InclusionExclusionCode> <IntervalBoundaryTypeCode>1</IntervalBoundaryTypeCode> <LowerBoundaryID>4358</LowerBoundaryID> </SelectionByID> </ServiceOrderSelectionByElements></n0:ServiceOrderByElementsQuery_SYNC> </soapenv:Body></soapenv:Envelope>";
-        doPostQuery(queryxml,String.format("%s:%s",username,password));
+        String queryxml = httpPostClient.file2xml("xmls/queryserviceorderin1.request.xml");
+        String queryrsxml = httpPostClient.doPostQuery(queryxml, String.format("%s:%s",username,password));
+        httpPostClient.xml2file("xmls/queryserviceorderin1.response.xml", queryrsxml);
 
-        String insertxml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:glob=\"http://sap.com/xi/SAPGlobal20/Global\"> <soapenv:Header/> <soapenv:Body> <glob:ServiceOrderBundleMaintainRequest_sync> <BasicMessageHeader/> <ServiceOrder> <Name languageCode=\"EN\">VK135马达上海20181205主题-muller</Name> <BuyerID>RPB181205CC0072</BuyerID> <SalesUnitParty> <PartyID>A-KS-R6-SH0303CD11</PartyID> </SalesUnitParty> <DateTime>2018-12-05T16:14:20Z</DateTime> <BuyerParty> <PartyID>SV009</PartyID> </BuyerParty> <InvoiceTerms actionCode=\"01\"> <ProposedInvoiceDate>2018-12-05</ProposedInvoiceDate> <InvoicingBlockingReasonCode>01</InvoicingBlockingReasonCode> </InvoiceTerms> <Item> <ID>10</ID> <ItemProduct> <ProductID>60631</ProductID> </ItemProduct> <Quantity unitCode=\"HUR\">5.0</Quantity> </Item> <Item> <ID>20</ID> <ItemProduct> <ProductID>60631</ProductID> </ItemProduct> <Quantity unitCode=\"HUR\">1.0</Quantity> </Item> </ServiceOrder> </glob:ServiceOrderBundleMaintainRequest_sync> </soapenv:Body></soapenv:Envelope>";
-        doPostInsert(insertxml,String.format("%s:%s",username,password));
+        String insertxml = httpPostClient.file2xml("xmls/manageserviceorderin4.request.xml");
+        String insertrsxml = httpPostClient.doPostInsert(insertxml,String.format("%s:%s",username,password));
+        httpPostClient.xml2file("xmls/manageserviceorderin4.response.xml", insertrsxml);
 
     }
 
-    public static void doPostInsert(String ...args){
+    public String file2xml(String resource) throws IOException {
+        return FileUtils.readFileToString(new File(getClass().getClassLoader().getResource(resource).getFile()), "UTF-8");
+    }
 
+    public void xml2file(String resource, String content) throws IOException {
+        FileUtils.writeStringToFile(new File(getClass().getClassLoader().getResource(resource).getFile()) ,content,"UTF-8");
+    }
+
+    public String doPostInsert(String ...args){
+        StringBuilder result = new StringBuilder();
         String xml = args[0];
         String host = "my337109.sapbydesign.com";
         String endPoint = "https://%s/sap/bc/srt/scs/sap/manageserviceorderin4?sap-vhost=my337109.sapbydesign.com";
@@ -76,7 +93,8 @@ public class HttpPostClient {
             CloseableHttpResponse response = httpclient.execute(post);
             try {
                 System.out.println(response.getStatusLine());
-                System.out.println(EntityUtils.toString(response.getEntity()));
+                //System.out.println(EntityUtils.toString(response.getEntity()));
+                result.append(EntityUtils.toString(response.getEntity()));
             } finally {
                 response.close();
             }
@@ -90,10 +108,13 @@ public class HttpPostClient {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            return result.toString();
         }
     }
 
-    public static void doPostQuery(String ...args){
+    public String doPostQuery(String ...args){
+        StringBuilder result = new StringBuilder();
         String xml = args[0];
         String host = "my337109.sapbydesign.com";
         String endPoint = "https://%s/sap/bc/srt/scs/sap/queryserviceorderin1?sap-vhost=my337109.sapbydesign.com";
@@ -137,7 +158,8 @@ public class HttpPostClient {
             CloseableHttpResponse response = httpclient.execute(post);
             try {
                 System.out.println(response.getStatusLine());
-                System.out.println(EntityUtils.toString(response.getEntity()));
+                //System.out.println(EntityUtils.toString(response.getEntity()));
+                result.append(EntityUtils.toString(response.getEntity()));
             } finally {
                 response.close();
             }
@@ -151,6 +173,8 @@ public class HttpPostClient {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            return result.toString();
         }
     }
 }
