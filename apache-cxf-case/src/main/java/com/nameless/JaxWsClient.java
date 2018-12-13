755b054ed8d501f1e024b0b92d64b3aa6159ad34
ken <kenshang@ehsure.com>
2018-12-13 17:30:33 +0800

    调用 查询 sap订单代码 main方法 生成

--- apache-cxf-case/src/main/java/com/nameless/JaxWsClient.java
+++ apache-cxf-case/src/main/java/com/nameless/JaxWsClient.java
@@ -1,146 +1,146 @@
-package com.nameless;
-
-import com.alibaba.fastjson.JSONObject;
-import com.sap.xi.ap.common.gdt.*;
-import com.sap.xi.ap.crm.global.*;
-import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-
-import javax.xml.datatype.XMLGregorianCalendar;
-import java.math.BigDecimal;
-
-/**
- * 有报错的类需要线 package
- * Created by boysz on 2018/11/16.
- */
-@Slf4j
-public class JaxWsClient {
-
-    /**
-     * END_POINT from SOAPUI
-     */
-    private final static String END_POINT = "https://my337109.sapbydesign.com/sap/bc/srt/scs/sap/manageserviceorderin4?sap-vhost=my337109.sapbydesign.com";
-
-
-    public static void main(String[] args) throws StandardFaultMessage {
-
-
-        String username = System.getProperty("username");
-        String password = System.getProperty("password");
-        System.out.println(String.format("username=%s,password=%s.",username,password));
-
-        /**
-         * 如果 classpath:cxf.xml / classpath:META-INF/cxf.xml
-         * 存在蔡荣如下方式创建.
-         * org.apache.cxf.bus.spring.BusApplicationContext getConfigResources
-         * 不存在采用如下方式创建.
-         * org.apache.cxf.wsdl.service.factory.ReflectionServiceFactoryBean buildServiceFromClass
-         */
-        JaxWsProxyFactoryBean clientFactory = new JaxWsProxyFactoryBean();
-        clientFactory.setAddress(END_POINT);
-        clientFactory.setServiceClass(ManageServiceOrderIn.class);
-        clientFactory.setUsername(username);
-        clientFactory.setPassword(password);
-
-
-        ManageServiceOrderIn manageServiceOrderIn = (ManageServiceOrderIn) clientFactory.create();
-
-        /**
-         * 参数拼写
-         */
-        ServiceOrderBundleMaintainRequest request = new ServiceOrderBundleMaintainRequest();
-        BusinessDocumentBasicMessageHeader header = new BusinessDocumentBasicMessageHeader();
-        ServiceOrderMaintainRequest serviceOrderMaintainRequest = new ServiceOrderMaintainRequest();
-
-        /**
-         * Name
-         */
-        EXTENDEDName extendedName = new EXTENDEDName();
-        extendedName.setLanguageCode("EN");
-        extendedName.setValue("VK135马达上海20181205主题-muller");
-        serviceOrderMaintainRequest.setName(extendedName);
-
-        /**
-         * BuyerID
-         */
-        BusinessTransactionDocumentID buyerID = new BusinessTransactionDocumentID();
-        buyerID.setValue("RPB181205CC0072");
-        serviceOrderMaintainRequest.setBuyerID(buyerID); //DEMO BUYER
-
-        /**
-         * SalesUnitParty
-         */
-        PartyID salesUnitPartyPartyID = new PartyID();
-        salesUnitPartyPartyID.setValue("A-KS-R6-SH0303CD11");
-        ServiceOrderMaintainRequestPartyParty salesUnitParty = new ServiceOrderMaintainRequestPartyParty();
-        salesUnitParty.setPartyID(salesUnitPartyPartyID);
-        serviceOrderMaintainRequest.setSalesUnitParty(salesUnitParty);
-
-        /**
-         * DateTime
-         */
-        XMLGregorianCalendar dateTime = new XMLGregorianCalendarImpl();
-        serviceOrderMaintainRequest.setDateTime(dateTime);
-
-        /**
-         * BuyerParty
-         */
-        PartyID buyerPartyPartyId = new PartyID();
-        buyerPartyPartyId.setValue("SV009");
-        ServiceOrderMaintainRequestPartyParty buyerParty = new ServiceOrderMaintainRequestPartyParty();
-        buyerParty.setPartyID(buyerPartyPartyId);
-        serviceOrderMaintainRequest.setBuyerParty(buyerParty);
-
-        /**
-         * InvoiceTerms
-         */
-        XMLGregorianCalendar proposedInvoiceDate = new XMLGregorianCalendarImpl();
-        ServiceOrderMaintainRequestInvoiceTerms invoiceTerms = new ServiceOrderMaintainRequestInvoiceTerms();
-        invoiceTerms.setActionCode("01");
-        invoiceTerms.setInvoicingBlockingReasonCode("01");
-        invoiceTerms.setProposedInvoiceDate(proposedInvoiceDate);
-        serviceOrderMaintainRequest.setInvoiceTerms(invoiceTerms);
-
-        /**
-         * Item
-         */
-        NOCONVERSIONProductID itemItemProductProductId0 = new NOCONVERSIONProductID();
-        itemItemProductProductId0.setValue("60631");
-
-        ServiceOrderMaintainRequestBundleItemProduct itemProduct0 = new ServiceOrderMaintainRequestBundleItemProduct();
-        itemProduct0.setProductID(itemItemProductProductId0);
-
-        Quantity itemQuantity = new Quantity();
-        itemQuantity.setUnitCode("HUR");
-        itemQuantity.setValue(new BigDecimal("5.0"));
-
-        ServiceOrderMaintainRequestItems item0 = new ServiceOrderMaintainRequestItems();
-        item0.setID("10");
-        item0.setItemProduct(itemProduct0);
-        item0.setQuantity(itemQuantity);
-        serviceOrderMaintainRequest.getItem().add(item0);
-
-
-        /**
-         * SerInvoiceTypeCI
-         */
-        serviceOrderMaintainRequest.setSerInvoiceTypeCI("102");
-
-        /**
-         * SerInvoiceCustomerAddressTelCI
-         */
-        serviceOrderMaintainRequest.setSerInvoiceCustomerAddressTelCI("13564549041");
-
-        /**
-         * SerInvoiceCustomerTaxNumberCI
-         */
-        serviceOrderMaintainRequest.setSerInvoiceCustomerTaxNumberCI("13564549041");
-
-
-        request.getServiceOrder().add(serviceOrderMaintainRequest);
-        request.setBasicMessageHeader(header);
-        ServiceOrderBundleMaintainConfirmation result = manageServiceOrderIn.maintainBundle(request);
-        System.out.println(JSONObject.toJSONString(result,true));
-    }
-}
+//package com.nameless;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.sap.xi.ap.common.gdt.*;
+//import com.sap.xi.ap.crm.global.*;
+//import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+//
+//import javax.xml.datatype.XMLGregorianCalendar;
+//import java.math.BigDecimal;
+//
+///**
+// * 有报错的类需要线 package
+// * Created by boysz on 2018/11/16.
+// */
+//@Slf4j
+//public class JaxWsClient {
+//
+//    /**
+//     * END_POINT from SOAPUI
+//     */
+//    private final static String END_POINT = "https://my337109.sapbydesign.com/sap/bc/srt/scs/sap/manageserviceorderin4?sap-vhost=my337109.sapbydesign.com";
+//
+//
+//    public static void main(String[] args) throws StandardFaultMessage {
+//
+//
+//        String username = System.getProperty("username");
+//        String password = System.getProperty("password");
+//        System.out.println(String.format("username=%s,password=%s.",username,password));
+//
+//        /**
+//         * 如果 classpath:cxf.xml / classpath:META-INF/cxf.xml
+//         * 存在蔡荣如下方式创建.
+//         * org.apache.cxf.bus.spring.BusApplicationContext getConfigResources
+//         * 不存在采用如下方式创建.
+//         * org.apache.cxf.wsdl.service.factory.ReflectionServiceFactoryBean buildServiceFromClass
+//         */
+//        JaxWsProxyFactoryBean clientFactory = new JaxWsProxyFactoryBean();
+//        clientFactory.setAddress(END_POINT);
+//        clientFactory.setServiceClass(ManageServiceOrderIn.class);
+//        clientFactory.setUsername(username);
+//        clientFactory.setPassword(password);
+//
+//
+//        ManageServiceOrderIn manageServiceOrderIn = (ManageServiceOrderIn) clientFactory.create();
+//
+//        /**
+//         * 参数拼写
+//         */
+//        ServiceOrderBundleMaintainRequest request = new ServiceOrderBundleMaintainRequest();
+//        BusinessDocumentBasicMessageHeader header = new BusinessDocumentBasicMessageHeader();
+//        ServiceOrderMaintainRequest serviceOrderMaintainRequest = new ServiceOrderMaintainRequest();
+//
+//        /**
+//         * Name
+//         */
+//        EXTENDEDName extendedName = new EXTENDEDName();
+//        extendedName.setLanguageCode("EN");
+//        extendedName.setValue("VK135马达上海20181205主题-muller");
+//        serviceOrderMaintainRequest.setName(extendedName);
+//
+//        /**
+//         * BuyerID
+//         */
+//        BusinessTransactionDocumentID buyerID = new BusinessTransactionDocumentID();
+//        buyerID.setValue("RPB181205CC0072");
+//        serviceOrderMaintainRequest.setBuyerID(buyerID); //DEMO BUYER
+//
+//        /**
+//         * SalesUnitParty
+//         */
+//        PartyID salesUnitPartyPartyID = new PartyID();
+//        salesUnitPartyPartyID.setValue("A-KS-R6-SH0303CD11");
+//        ServiceOrderMaintainRequestPartyParty salesUnitParty = new ServiceOrderMaintainRequestPartyParty();
+//        salesUnitParty.setPartyID(salesUnitPartyPartyID);
+//        serviceOrderMaintainRequest.setSalesUnitParty(salesUnitParty);
+//
+//        /**
+//         * DateTime
+//         */
+//        XMLGregorianCalendar dateTime = new XMLGregorianCalendarImpl();
+//        serviceOrderMaintainRequest.setDateTime(dateTime);
+//
+//        /**
+//         * BuyerParty
+//         */
+//        PartyID buyerPartyPartyId = new PartyID();
+//        buyerPartyPartyId.setValue("SV009");
+//        ServiceOrderMaintainRequestPartyParty buyerParty = new ServiceOrderMaintainRequestPartyParty();
+//        buyerParty.setPartyID(buyerPartyPartyId);
+//        serviceOrderMaintainRequest.setBuyerParty(buyerParty);
+//
+//        /**
+//         * InvoiceTerms
+//         */
+//        XMLGregorianCalendar proposedInvoiceDate = new XMLGregorianCalendarImpl();
+//        ServiceOrderMaintainRequestInvoiceTerms invoiceTerms = new ServiceOrderMaintainRequestInvoiceTerms();
+//        invoiceTerms.setActionCode("01");
+//        invoiceTerms.setInvoicingBlockingReasonCode("01");
+//        invoiceTerms.setProposedInvoiceDate(proposedInvoiceDate);
+//        serviceOrderMaintainRequest.setInvoiceTerms(invoiceTerms);
+//
+//        /**
+//         * Item
+//         */
+//        NOCONVERSIONProductID itemItemProductProductId0 = new NOCONVERSIONProductID();
+//        itemItemProductProductId0.setValue("60631");
+//
+//        ServiceOrderMaintainRequestBundleItemProduct itemProduct0 = new ServiceOrderMaintainRequestBundleItemProduct();
+//        itemProduct0.setProductID(itemItemProductProductId0);
+//
+//        Quantity itemQuantity = new Quantity();
+//        itemQuantity.setUnitCode("HUR");
+//        itemQuantity.setValue(new BigDecimal("5.0"));
+//
+//        ServiceOrderMaintainRequestItems item0 = new ServiceOrderMaintainRequestItems();
+//        item0.setID("10");
+//        item0.setItemProduct(itemProduct0);
+//        item0.setQuantity(itemQuantity);
+//        serviceOrderMaintainRequest.getItem().add(item0);
+//
+//
+//        /**
+//         * SerInvoiceTypeCI
+//         */
+//        serviceOrderMaintainRequest.setSerInvoiceTypeCI("102");
+//
+//        /**
+//         * SerInvoiceCustomerAddressTelCI
+//         */
+//        serviceOrderMaintainRequest.setSerInvoiceCustomerAddressTelCI("13564549041");
+//
+//        /**
+//         * SerInvoiceCustomerTaxNumberCI
+//         */
+//        serviceOrderMaintainRequest.setSerInvoiceCustomerTaxNumberCI("13564549041");
+//
+//
+//        request.getServiceOrder().add(serviceOrderMaintainRequest);
+//        request.setBasicMessageHeader(header);
+//        ServiceOrderBundleMaintainConfirmation result = manageServiceOrderIn.maintainBundle(request);
+//        System.out.println(JSONObject.toJSONString(result,true));
+//    }
+//}
--- apache-cxf-case/src/main/java/com/nameless/JaxWsClient1.java
+++ apache-cxf-case/src/main/java/com/nameless/JaxWsClient1.java
@@ -1,7 +1,18 @@
 package com.nameless;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sap.xi.a1s.global.*;
+import com.sap.xi.ap.common.gdt.BusinessTransactionDocumentID;
+import com.sap.xi.ap.common.gdt.QueryProcessingConditions;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 有报错的类需要线 package
@@ -10,13 +21,22 @@ import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 @Slf4j
 public class JaxWsClient1 {
 
+    private final static Gson gson = new GsonBuilder()
+//            .excludeFieldsWithoutExposeAnnotation() //不导出实体中没有用@Expose注解的属性
+            .enableComplexMapKeySerialization() //支持Map的key为复杂对象的形式
+            .serializeNulls()
+//            .setDateFormat("yyyy-MM-dd HH:mm:ss:SSS")//时间转化为特定格式
+//            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)//会把字段首字母大写,注:对于实体上使用了@SerializedName注解的不会生效.
+            .setPrettyPrinting() //对json结果格式化.
+            .create();
+
     /**
      * END_POINT from SOAPUI
      */
     private final static String END_POINT = "https://my337109.sapbydesign.com/sap/bc/srt/scs/sap/queryserviceorderin1?sap-vhost=my337109.sapbydesign.com";
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws StandardFaultMessage {
 
 
         String username = "_SERVICESORD";
@@ -32,8 +52,55 @@ public class JaxWsClient1 {
          */
         JaxWsProxyFactoryBean clientFactory = new JaxWsProxyFactoryBean();
         clientFactory.setAddress(END_POINT);
+        clientFactory.setServiceClass(QueryServiceOrderIn.class);
+        clientFactory.setUsername(username);
+        clientFactory.setPassword(password);
+
+        // service
+        QueryServiceOrderIn queryServiceOrderIn = (QueryServiceOrderIn) clientFactory.create();
+
+        /**
+         * 参数拼写
+         */
+
+        // ServiceOrderByElementsQuery_SYNC
+        ServiceOrderByElementsQueryMessageSYNC request = new ServiceOrderByElementsQueryMessageSYNC();
+
+        // ServiceOrderSelectionByElements
+        ServiceOrderByElementsQuerySelectionByElementS serviceOrderSelectionByElements = new ServiceOrderByElementsQuerySelectionByElementS();
+        // SelectionByID
+        ServiceOrderByElementsQuerySelectionByid selectionByIDItem = new ServiceOrderByElementsQuerySelectionByid();
+        selectionByIDItem.setInclusionExclusionCode("I");
+        selectionByIDItem.setIntervalBoundaryTypeCode("1");
+        BusinessTransactionDocumentID lowerBoundaryID = new BusinessTransactionDocumentID();
+        lowerBoundaryID.setValue("4321");
+        selectionByIDItem.setLowerBoundaryID(lowerBoundaryID);
+        // serviceOrderSelectionByElements set value add list
+        serviceOrderSelectionByElements.getSelectionByID().add(selectionByIDItem);
 
 
-//        System.out.println(JSONObject.toJSONString(result,true));
+        // ProcessingConditions
+        QueryProcessingConditions processingConditions = new QueryProcessingConditions();
+        processingConditions.setQueryHitsMaximumNumberValue(30);
+        processingConditions.setQueryHitsUnlimitedIndicator(false);
+
+
+
+        // req set value
+        request.setServiceOrderSelectionByElements(serviceOrderSelectionByElements);
+        request.setProcessingConditions(processingConditions);
+
+
+        ServiceOrderByElementsResponseMessageSynC result =  queryServiceOrderIn.findByElements(request);
+        String jsonStr = gson.toJson(result);
+        System.out.println(jsonStr);
+
+//        String jsonStr = JSONObject.toJSONString(result, SerializerFeature.IgnoreNonFieldGetter);
+//        String formatStr = JSONObject.toJSONString(jsonStr,true);
+//        System.out.println(formatStr);
+
+
+
+
     }
 }
